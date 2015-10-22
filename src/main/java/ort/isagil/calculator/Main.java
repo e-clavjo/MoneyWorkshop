d88368bfe734c0a140e33c0349bfded1f77245ee
e-clavjo <clavijoeliana@gmail.com>
2015-10-22 21:56:24 -0200

    Update Main.java

--- src/main/java/ort/isagil/calculator/Main.java
+++ src/main/java/ort/isagil/calculator/Main.java
@@ -3,6 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+ /**/
 package ort.isagil.calculator;
 
 import ort.isagil.calculator.model.*;
